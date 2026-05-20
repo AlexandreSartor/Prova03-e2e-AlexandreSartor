9b63cef36cd0ab61c9b37d378ab32acd0c5187fb
Alexandre Sartor <xandesartorteixeira@gmail.com>
2026-05-20 18:04:47 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,16 +9,25 @@ const config: PlaywrightTestConfig = {
 
   use: {
     trace: 'retain-on-failure',
+
     locale: 'pt-BR',
+
     headless: true,
+
+    userAgent:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+
     viewport: { width: 1280, height: 720 },
+
     ignoreHTTPSErrors: true,
+
     screenshot: 'only-on-failure',
+
     video: 'off'
   },
 
   expect: {
-    timeout: 10000
+    timeout: 15000
   },
 
   reporter: [
